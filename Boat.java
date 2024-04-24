2acfc05f62b5cf95cf9dd75a397ec857f4a45539
NICooper <nicholas.i.cooper@gmail.com>
2024-04-23 23:26:12 -0400

    updated default section sizes

--- Boat.java
+++ Boat.java
@@ -3,11 +3,11 @@ import java.util.List;
 public class Boat extends Vehicle {
     public Boat(String vehicleId, String model, Company company) {
         super(vehicleId, model, company);
-        sections.add(new BoatSection(1, BoatSectionType.I));
-        sections.add(new BoatSection(1, BoatSectionType.O));
-        sections.add(new BoatSection(1, BoatSectionType.S));
-        sections.add(new BoatSection(1, BoatSectionType.F));
-        sections.add(new BoatSection(1, BoatSectionType.D));
+        sections.add(new BoatSection(10, BoatSectionType.I));
+        sections.add(new BoatSection(15, BoatSectionType.O));
+        sections.add(new BoatSection(20, BoatSectionType.S));
+        sections.add(new BoatSection(30, BoatSectionType.F));
+        sections.add(new BoatSection(40, BoatSectionType.D));
     }
 
     @Override
--- Train.java
+++ Train.java
@@ -1,8 +1,8 @@
 public class Train extends Vehicle {
     public Train(String vehicleId, String model, Company company) {
         super(vehicleId, model, company);
-        sections.add(new TrainSection(1, TrainSectionType.P));
-        sections.add(new TrainSection(1, TrainSectionType.E));
+        sections.add(new TrainSection(50, TrainSectionType.P));
+        sections.add(new TrainSection(90, TrainSectionType.E));
     }
 
     @Override
